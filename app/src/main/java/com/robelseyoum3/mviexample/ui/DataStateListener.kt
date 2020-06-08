4bf60e35f173ff1b2ef3026173e09666ff7707f1
Robel <robelseyoum3@gmail.com>
2020-06-08 18:52:45 +0100

    adding DataState Listener

--- app/src/main/java/com/robelseyoum3/mviexample/ui/DataStateListener.kt
+++ app/src/main/java/com/robelseyoum3/mviexample/ui/DataStateListener.kt
@@ -1,4 +1,7 @@
 package com.robelseyoum3.mviexample.ui
 
+import com.robelseyoum3.mviexample.util.DataState
+
 interface DataStateListener {
+    fun onDataStateChange(dataState: DataState<*>?) //type invariant or any type
 }
